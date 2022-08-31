3d40b5ca6d435eb4675193c2b9a1c8c86baabaf2
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-08-31 18:01:27 +1000

    lab submission

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+//declare employee object
+const employee = {
+    name: "Ben Gross",
+    streetAddress: "11 Beverly Hills"
+};
+
+//non-destructive
+//returns an employee with the original key value pairs and the new key value pair
+//it does not modify the original employee, but rather returns a clone with the new data
+//used the spread operator
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+//destructive
+//updates `employee` with the given `key` and `value` (it is destructuve) and returns the entire updated employee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+//deletes `key` from a clone of employee and returns the new employee (it is non-destructive)
+//does not modify the original employee (it is non-destructuve)
+//used the spread operator
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+//modifies the original employee
+//returns employee without the deleted key/value pair
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
